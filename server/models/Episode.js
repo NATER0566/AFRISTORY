9aadb7973265877316061c25917546fe77a4dc03
NATER MBASHAU <nmbashau@gmail.com>
2026-09-22 00:15:52 +0100

    Update Episode.js

--- server/models/Episode.js
+++ server/models/Episode.js
@@ -97,7 +97,6 @@ const episodeSchema = new mongoose.Schema(
       type: Number,
       default: 0,
     },
-    // PHASE 5.1 CLEANUP: totalAdUnlocks removed permanently.
     averageWatchTime: {
       type: Number,
       default: 0, // in seconds
